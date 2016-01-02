ef8299c4621e2a29d96fb20ded491ea81482f392
Amar Shah <amar47shah@gmail.com>
2016-01-02 17:13:52 -0500

    Allow illegible OCR and represent as '?'
    Test case added

--- Parser.hs
+++ Parser.hs
@@ -1,41 +1,36 @@
 {-# OPTIONS_GHC -Wall #-}
 module Parser (assemble, parse) where
 
-import Data.Maybe (fromMaybe)
-
 type Chunk = [String]
 type OCR = (String, String, String)
 type Digit = Char
 type Number = [Digit]
 
-assemble :: [Maybe Number] -> String
-assemble = unlines . map (fromMaybe "")
+assemble :: [Number] -> String
+assemble = unlines
 
-parse :: String -> [Maybe Number]
-parse = map parseNumber . chunks
+parse :: String -> [Number]
+parse = map parseDigits . chunks
 
-parseNumber :: Chunk -> Maybe Number
-parseNumber = sequence . parseDigits
-
-parseDigits :: Chunk -> [Maybe Digit]
+parseDigits :: Chunk -> Number
 parseDigits c@(_:_:_:_:_) = map fromOCR $ zip3 x y z
   where [x, y, z] = map (splitEvery 3) . take 3 $ c
 parseDigits _ = []
 
-fromOCR :: OCR -> Maybe Digit
+fromOCR :: OCR -> Digit
 fromOCR o =
- case o of
-   (" _ ","| |","|_|") -> Just '0'
-   ("   ","  |","  |") -> Just '1'
-   (" _ "," _|","|_ ") -> Just '2'
-   (" _ "," _|"," _|") -> Just '3'
-   ("   ","|_|","  |") -> Just '4'
-   (" _ ","|_ "," _|") -> Just '5'
-   (" _ ","|_ ","|_|") -> Just '6'
-   (" _ ","  |","  |") -> Just '7'
-   (" _ ","|_|","|_|") -> Just '8'
-   (" _ ","|_|"," _|") -> Just '9'
-   _ -> Nothing
+  case o of
+    (" _ ","| |","|_|") -> '0'
+    ("   ","  |","  |") -> '1'
+    (" _ "," _|","|_ ") -> '2'
+    (" _ "," _|"," _|") -> '3'
+    ("   ","|_|","  |") -> '4'
+    (" _ ","|_ "," _|") -> '5'
+    (" _ ","|_ ","|_|") -> '6'
+    (" _ ","  |","  |") -> '7'
+    (" _ ","|_|","|_|") -> '8'
+    (" _ ","|_|"," _|") -> '9'
+    _                   -> '?'
 
 chunks :: String -> [Chunk]
 chunks = splitEvery 4 . lines
