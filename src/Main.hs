c3758eecc6a6aa2e38ed977ba8f04dcc2c343912
Amar Shah <amar47shah@gmail.com>
2016-01-27 01:30:43 -0500

    Remove unnecesssary bind and return

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,14 @@ module Main where
 
 import Parser (assemble, parse)
 import qualified Data.ByteString.Lazy.Char8
-       as BS (pack, readFile, unpack, writeFile)
+       as BS (ByteString, pack, readFile, unpack, writeFile)
 
 main :: IO ()
 main =
-  BS.readFile "data/input.txt" >>=
-    return . BS.pack . assemble . parse . BS.unpack >>=
+  translate <$>
+    BS.readFile "data/input.txt" >>=
       BS.writeFile "data/output.txt" >>
         putStrLn "Done"
+
+translate :: BS.ByteString -> BS.ByteString
+translate = BS.pack . assemble . parse . BS.unpack
